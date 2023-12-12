1b9c698e1c910bf1c22021e955fca63e5957c0f3
yoonjuho <dbswn990@naver.com>
2023-12-13 02:02:10 +0900

    refactor(Level): 각 레벨들은 미션을 다양하게 가지고 있다.

--- src/main/java/pairmatching/model/Level.java
+++ src/main/java/pairmatching/model/Level.java
@@ -1,17 +1,22 @@
 package pairmatching.model;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum Level {
-    LEVEL1("레벨1"),
-    LEVEL2("레벨2"),
-    LEVEL3("레벨3"),
-    LEVEL4("레벨4"),
-    LEVEL5("레벨5");
+    LEVEL1("레벨1", Arrays.asList(Mission.RACING_CAR, Mission.LOTTO, Mission.BASEBALL)),
+    LEVEL2("레벨2", Arrays.asList(Mission.SHOPPING_BASKET, Mission.PAYMENT, Mission.SUBWAY_ROUTE_MAP)),
+    LEVEL3("레벨3", Arrays.asList()),
+    LEVEL4("레벨4", Arrays.asList(Mission.PERFORMANCE_IMPROVEMENT, Mission.DEPLOY)),
+    LEVEL5("레벨5", Arrays.asList());
 
     private String name;
+    private List<Mission> missions;
 
-    Level(String name) {
+    Level(String name, List<Mission> missions) {
         this.name = name;
+        this.missions = missions;
+
     }
 
-    // 추가 기능 구현
 }
