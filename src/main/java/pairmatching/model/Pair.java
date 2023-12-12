2094bce11c7121a76b9d6b0b1691c39a64b97fef
yoonjuho <dbswn990@naver.com>
2023-12-13 02:21:19 +0900

    refactor(Pair): 페어의 순서는 보장된다.

--- src/main/java/pairmatching/model/Pair.java
+++ src/main/java/pairmatching/model/Pair.java
@@ -1,19 +1,19 @@
 package pairmatching.model;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Pair {
     private final int MIN_SIZE = 2;
     private final int MAX_SIZE = 3;
-    private final Set<Crew> pair;
+    private final List<Crew> pair;
 
-    public Pair(Set<Crew> pair) {
+    public Pair(List<Crew> pair) {
         validateSize(pair);
-        this.pair = new HashSet<>(pair);
+        this.pair = new ArrayList<>(pair);
     }
 
-    private void validateSize(Set<Crew> pair){
+    private void validateSize(List<Crew> pair){
         if (pair.size() != MIN_SIZE && pair.size() != MAX_SIZE){
             throw new IllegalArgumentException("페어는 2인 혹은 3인으로 구성되어야 합니다.");
         }
--- src/main/java/pairmatching/model/PairMatching.java
+++ src/main/java/pairmatching/model/PairMatching.java
@@ -3,17 +3,17 @@ package pairmatching.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class PairMatching {
 
-    public Set<Crew> crew(Course course, List<String> crewNames){
+    public List<Crew> crew(Course course, List<String> crewNames){
         List<String> shuffledCrew = Randoms.shuffle(crewNames);
         return shuffledCrew.stream()
                 .map(crew -> new Crew(course, crew))
-                .collect(Collectors.toSet());
+                .collect(Collectors.toList());
     }
+
 
 
 }
