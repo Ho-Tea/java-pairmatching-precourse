1be682ac6211cf1183c15a8c676e4776eaa70a64
yoonjuho <dbswn990@naver.com>
2023-12-13 02:55:37 +0900

    feat(Pairs): 다수의 페어로 구성된다.

---
+++ src/main/java/pairmatching/model/Pairs.java
@@ -0,0 +1,15 @@
+package pairmatching.model;
+
+import java.util.List;
+
+public class Pairs {
+    private List<Pair> pairs;
+
+    public Pairs(List<Pair> pairs) {
+        this.pairs = pairs;
+    }
+
+    public void has(){
+
+    }
+}
