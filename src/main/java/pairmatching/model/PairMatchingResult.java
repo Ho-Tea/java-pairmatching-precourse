7a28ae21e96bb3db60c549b3c7e346482c804e69
yoonjuho <dbswn990@naver.com>
2023-12-12 19:15:02 +0900

    refactor(PairMatchingResult): 미션과 미션에 매칭된 페어들로 정의할 수 있다.

--- src/main/java/pairmatching/model/PairMatchingResult.java
+++ src/main/java/pairmatching/model/PairMatchingResult.java
@@ -3,9 +3,11 @@ package pairmatching.model;
 import java.util.List;
 
 public class PairMatchingResult {
+    private final Mission mission;
     private final List<Pair> matchResult;
 
-    public PairMatchingResult(List<Pair> matchResult) {
+    public PairMatchingResult(Mission mission, List<Pair> matchResult) {
+        this.mission = mission;
         this.matchResult = matchResult;
     }
 }
