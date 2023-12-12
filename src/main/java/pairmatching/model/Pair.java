e4fcf7c81f8753bea51d2201155f8e879b6b8535
yoonjuho <dbswn990@naver.com>
2023-12-13 03:40:50 +0900

    feat(Pair): 동등성 비교 추가

--- src/main/java/pairmatching/model/Pair.java
+++ src/main/java/pairmatching/model/Pair.java
@@ -2,6 +2,7 @@ package pairmatching.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Pair {
     private final int MIN_SIZE = 2;
@@ -17,5 +18,18 @@ public class Pair {
         if (pair.size() != MIN_SIZE && pair.size() != MAX_SIZE){
             throw new IllegalArgumentException("페어는 2인 혹은 3인으로 구성되어야 합니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair pair1 = (Pair) o;
+        return MIN_SIZE == pair1.MIN_SIZE && MAX_SIZE == pair1.MAX_SIZE && Objects.equals(pair, pair1.pair);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(MIN_SIZE, MAX_SIZE, pair);
     }
 }
