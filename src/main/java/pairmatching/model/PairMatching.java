fb190044efe1261270f61dfcc658dabb14cf6165
yoonjuho <dbswn990@naver.com>
2023-12-12 20:08:51 +0900

    feat(PairMatching): 미션을 함께 수행할 페어를 매칭한다.

--- src/main/java/pairmatching/model/PairMatching.java
+++ src/main/java/pairmatching/model/PairMatching.java
@@ -1,8 +1,9 @@
 package pairmatching.model;
 
-import java.util.EnumMap;
-import java.util.List;
-import java.util.Map;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PairMatching {
     private Map<Level, List<PairMatchingResult>> pairMatchingResults;
@@ -12,6 +13,37 @@ public class PairMatching {
     }
 
 
+    public void match(Course course, List<String> crewNames){
+        List<Pair> pairs = makePair(course, crewNames);
+
+
+
+    }
+
+    private List<Pair> makePair(Course course, List<String> crewNames){
+        List<String> shuffledCrew = Randoms.shuffle(crewNames);
+        List<Crew> crews = shuffledCrew.stream()
+                .map(crewName -> new Crew(course, crewName))
+                .collect(Collectors.toList());
+        if(crews.size() % 2 == 0){
+            return makeEvenPair(crews.size()/2, new ArrayList<>(crews));
+        }
+        return makeOddPair(crews.size()/2 - 1, new ArrayList<>(crews));
+    }
+
+    private List<Pair> makeEvenPair(int number, List<Crew> crews){
+        List<Pair> pairs = new ArrayList<>();
+        for(int i = 0; i < number; i++){
+            pairs.add(new Pair(Arrays.asList(crews.remove(0), crews.remove(0))));
+        }
+        return pairs;
+    }
+
+    private List<Pair> makeOddPair(int number, List<Crew> crews){
+        List<Pair> pairs = makeEvenPair(number, crews);
+        pairs.add(new Pair(Arrays.asList(crews.remove(0), crews.remove(0), crews.remove(0))));
+        return pairs;
+    }
 
 
 
