1f01c65f6e85575d5db6e15959783530b6a1eae8
yoonjuho <dbswn990@naver.com>
2023-12-12 20:16:30 +0900

    refactor(PairMatchingResult): 객체의 동등성 재정의

--- src/main/java/pairmatching/model/PairMatchingResult.java
+++ src/main/java/pairmatching/model/PairMatchingResult.java
@@ -1,6 +1,7 @@
 package pairmatching.model;
 
 import java.util.List;
+import java.util.Objects;
 
 public class PairMatchingResult {
     private final Mission mission;
@@ -9,5 +10,18 @@ public class PairMatchingResult {
     public PairMatchingResult(Mission mission, List<Pair> matchResult) {
         this.mission = mission;
         this.matchResult = matchResult;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PairMatchingResult that = (PairMatchingResult) o;
+        return mission == that.mission && Objects.equals(matchResult, that.matchResult);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mission, matchResult);
     }
 }
