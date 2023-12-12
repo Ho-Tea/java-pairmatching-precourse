c75b6946a59f5b676c145aab9521130c2644e307
yoonjuho <dbswn990@naver.com>
2023-12-12 18:17:28 +0900

    feat(PairMatchingResult): 매칭된 페어들로 정의된다.

---
+++ src/main/java/pairmatching/model/PairMatchingResult.java
@@ -0,0 +1,11 @@
+package pairmatching.model;
+
+import java.util.List;
+
+public class PairMatchingResult {
+    private final List<Pair> matchResult;
+
+    public PairMatchingResult(List<Pair> matchResult) {
+        this.matchResult = matchResult;
+    }
+}
