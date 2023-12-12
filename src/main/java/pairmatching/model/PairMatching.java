c8f351422f044f90a7968ca94d81549bbf79ec4a
yoonjuho <dbswn990@naver.com>
2023-12-12 19:15:16 +0900

    refactor(PairMatching): 레벨과 해당 레벨 내의 페어 매칭 결과를 정의할 수 있다.

--- src/main/java/pairmatching/model/PairMatching.java
+++ src/main/java/pairmatching/model/PairMatching.java
@@ -5,11 +5,12 @@ import java.util.List;
 import java.util.Map;
 
 public class PairMatching {
-    private Map<Mission, List<PairMatchingResult>> pairMatchingResults;
+    private Map<Level, List<PairMatchingResult>> pairMatchingResults;
 
     public PairMatching() {
-        pairMatchingResults = new EnumMap<>(Mission.class);
+        pairMatchingResults = new EnumMap<>(Level.class);
     }
+
 
 
 
