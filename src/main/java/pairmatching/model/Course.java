3e5582fb850ca818abcedb39b6409a7331b246a9
yoonjuho <dbswn990@naver.com>
2023-12-13 18:57:28 +0900

    feat(Course): 이름으로 과정을 구분할 수 있다.

--- src/main/java/pairmatching/model/Course.java
+++ src/main/java/pairmatching/model/Course.java
@@ -1,5 +1,9 @@
 package pairmatching.model;
 
+import pairmatching.constant.Exception;
+
+import java.util.Arrays;
+
 public enum Course {
     BACKEND("백엔드"),
     FRONTEND("프론트엔드");
@@ -10,4 +14,14 @@ public enum Course {
         this.name = name;
     }
 
+    public static Course of(String target) {
+        return Arrays.stream(values())
+                .filter(course -> course.name.equals(target))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(Exception.COURSE.getMessage()));
+    }
+
+    public String getName() {
+        return name;
+    }
 }
