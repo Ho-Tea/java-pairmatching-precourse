662cd3864f71497fa22b4a683fb8c5e015a56224
yoonjuho <dbswn990@naver.com>
2023-12-13 19:24:16 +0900

    feat(OutputView): 페어 프로그래밍에 관련한 출력을 한다.

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,13 +1,40 @@
 package pairmatching.view;
 
-import pairmatching.model.PairMatchingFunction;
+import pairmatching.constant.Exception;
+import pairmatching.model.*;
+
+import java.util.stream.Collectors;
 
 public class OutputView {
 
-    public void printChooseFunction(){
+    public void printChooseFunction() {
         System.out.println("기능을 선택하세요.");
-        for(PairMatchingFunction pairMatchingFunction : PairMatchingFunction.values()){
+        for (FunctionType pairMatchingFunction : FunctionType.values()) {
             System.out.println(pairMatchingFunction.getSignature() + ". " + pairMatchingFunction.getName());
         }
+        System.out.println();
+    }
+
+    public void printCourseAndMission() {
+        System.out.println("#############################################");
+        System.out.println("과정: " + Course.BACKEND.getName() + " | " + Course.FRONTEND.getName());
+        System.out.println("미션:");
+        for (Level level : Level.values()) {
+            System.out.println("  -" + level.getName() + ":" + level.getMissions().stream().map(mission -> mission.getName()).collect(Collectors.joining(" | ")));
+        }
+        System.out.println("#############################################");
+
+    }
+
+    public void printMatchingResult(PairMatchingResult pairMatchingResult) {
+        System.out.println("페어 매칭 결과입니다.");
+        for (Pair pairs : pairMatchingResult.getPairs().getPairGroup()) {
+            System.out.println(pairs.getPair().stream().map(crew -> crew.getName()).collect(Collectors.joining(" : ")));
+        }
+        System.out.println();
+    }
+
+    public void printException(String message) {
+        System.out.println(Exception.ERROR_PREFIX.getMessage() + message);
     }
 }
