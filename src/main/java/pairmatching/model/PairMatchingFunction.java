04d42c1cbf195bc04b5a295cf071673a62ecc435
yoonjuho <dbswn990@naver.com>
2023-12-13 16:17:10 +0900

    feat(PairMatchingFunction): 페어 매칭 기능 자격을 부여한다.

--- src/main/java/pairmatching/model/PairMatchingFunction.java
+++ src/main/java/pairmatching/model/PairMatchingFunction.java
@@ -1,18 +1,5 @@
 package pairmatching.model;
 
-public enum PairMatchingFunction {
-    PAIR_MATCHING("페어 매칭"),
-    PAIR_INQUIRY("페어 조회"),
-    PAIR_INITIALIZATION("페어 초기화"),
-    QUIT("종료");
-
-    private String name;
-
-    PairMatchingFunction(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
+public interface PairMatchingFunction {
+    boolean isActive(FunctionType pairMatchingFunction);
 }
